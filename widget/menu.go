67ebd72a28f9a58c52c479cb4769f2cab842b6ec
njkhanh <njkhanh@gmail.com>
2020-09-12 03:21:40 +0700

    add strings utils

--- widget/menu.go
+++ widget/menu.go
@@ -1,14 +1,58 @@
-package widget
+package klib
 
-type Menu struct {
-	Text       string
-	Icon       string
-	Url        string
-	Route      string
-	Active     string
-	Activated  bool
-	IsHidden   bool
-	Permission string
-	Target     string
-	Children   []Menu
+import (
+	"regexp"
+	"strings"
+)
+
+var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+
+func ToSnakeCase(str string) string {
+	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+	return strings.ToLower(snake)
+}
+
+func ReplaceNth(s, old, new string, n int) string {
+	i := 0
+	for m := 1; m <= n; m++ {
+		x := strings.Index(s[i:], old)
+		if x < 0 {
+			break
+		}
+		i += x
+		if m == n {
+			return s[:i] + new + s[i+len(old):]
+		}
+		i += len(old)
+	}
+	return s
+}
+
+func TruncateStr(str string, num int) string {
+	s := []rune(str)
+	if len(s) > num {
+		s := []rune(str)
+
+		return string(s[0:num])
+	}
+
+	return str
+}
+
+func TrimStr(str string, start int) string {
+	s := []rune(str)
+	if len(s) > start {
+		s := []rune(str)
+
+		return string(s[start:])
+	}
+
+	return ""
+}
+
+// 26
+func FuzzySign(text string, n int) string {
+	return TrimStr(text, n) + TruncateStr(text, n)
 }
--- widget/paginator.go
+++ widget/paginator.go
@@ -1,4 +1,4 @@
-package widget
+package klib
 
 import (
 	"math"
